bb755d1301ed2ab36d2e3f03139956bc81458595
eastman19deni <deni12349075@gmail.com>
2025-02-16 17:41:07 +0300

    Changed Name in files

---
+++ src/components/Execises/Exercises.jsx
@@ -0,0 +1,13 @@
+
+function Exercises() {
+    const text = "addition text"
+  const title = "react"
+    return(
+      <div>
+       <h1 class="text" >Текст про {text}</h1>
+       <h2 class="title">не понимаю {title}</h2>
+      </div>
+    )
+}
+
+export default Exercises
